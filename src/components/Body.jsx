77faaaa5b0a8f2f7f3992e99daebf32a974e2e29
miriyan19425 <miriyan19425@gmail.com>
2025-06-06 23:35:52 +0300

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Main from './Main';
+
+function Body(){
+    return (
+        <div className="body">
+            <h1>Motorcycle Showroom</h1>
+            <h4>Reserve a Test Ride</h4>
+            <Main />
+        </div>
+    );
+}
+
+export default Body;
