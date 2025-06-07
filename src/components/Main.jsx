68529f2ac2a9f569d33be78e509c021e4d9e4cf0
miriyan19425 <miriyan19425@gmail.com>
2025-06-07 11:15:58 +0300

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import FormContainer from './FormContainer';
+
+function Main(){
+    return (
+        <main id="content">
+            <FormContainer />
+        </main>
+    );
+}
+
+export default Main;
