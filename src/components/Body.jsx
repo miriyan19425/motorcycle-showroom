0c4d2ead00feb04619a5df0f5f87b3ca7d5996d6
miriyan19425 <miriyan19425@gmail.com>
2025-07-05 23:21:19 +0300

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,9 @@
 import Main from './Main';
+import styles from './Body.module.css';
 
 function Body(){
     return (
-        <div className="body">
+        <div className={styles.body}>
             <h1>Motorcycle Showroom</h1>
             <h4>Reserve a Test Ride</h4>
             <Main />
