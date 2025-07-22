4f23275aa9914d8bdf4f207b98e5185d4ab22134
miriyan19425 <miriyan19425@gmail.com>
2025-07-22 22:41:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import Body from './components/Body'; 
+import TestRideForm from "./components/TestRideForm";
 
 function App() {
-   return <Body />;
+   return (
+        <div className="body">
+    <h1>Motorcycle Showroom</h1>
+    <h4>Reserve a Test Ride</h4>
+    <TestRideForm />
+    <div className="data-view">
+      <div className="preview">
+        <ul id="preview-list"></ul>
+      </div>
+      <div className="complete">
+        <ul id="complete-list"></ul>
+      </div>
+    </div>
+     
+  </div>
+   );
 }
 
 export default App;
