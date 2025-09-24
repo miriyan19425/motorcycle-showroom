4392415671cad7dfd0be3ae1e912ce5e2c54d696
miriyan19425 <miriyan19425@gmail.com>
2025-09-24 23:53:00 +0300

    Update TestRidePreview.jsx

--- src/components/TestRidePreview.jsx
+++ src/components/TestRidePreview.jsx
@@ -15,6 +15,7 @@ function TestRidePreview({testRideInput}){
                             <p>Color: {testRideInput.color}</p>
                             <p>Model: {testRideInput.model}</p>
                             <p>For: {testRideInput.name}</p>
+                            <p>Contact: {testRideInput.email}</p>
                             
                         </article>
                         <div>
